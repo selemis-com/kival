4bb3157429becf2d02567fd1d40ff1b20f8da911
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2026-08-30 20:20:43 +0200

    clean up

--- bin/kival/src/main.rs
+++ bin/kival/src/main.rs
@@ -89,23 +89,13 @@ pub enum Commands {
 }
 
 /// CLI client to interact with Kival.
-/// ~~~text
+/// ```ignore
 ///     __ __ __            __
 ///    / //_//_/_  ______ _/ /
 ///   / ,<  / / | / / __ `/ /
 ///  / /| |/ /| |/ / /_/ / /
 /// /_/ |_/_/ |___/\__,_/_/
-/// ~~~
-///
-/// Machine-readable usage:
-///
-/// - `kival schema` inspects the root command and its children.
-/// - `kival schema <COMMAND>` inspects a command or command group.
-/// - `kival schema <COMMAND> --full` emits a recursive command schema.
-/// - `kival <COMMAND> -O json` emits machine-readable output.
-///
-/// For agent authentication, set `KIVAL_URL` and `KIVAL_API_KEY`, then invoke the desired
-/// command.
+/// ```
 #[derive(Debug, Parser)]
 #[argx(name = "kival", version = SHORT_VERSION, long_version = LONG_VERSION, schema)]
 pub struct Cli {
--- bin/kivald/src/main.rs
+++ bin/kivald/src/main.rs
@@ -90,13 +90,13 @@ pub enum Commands {
 }
 
 /// CLI server to host Kival.
-/// ~~~text
+/// ```ignore
 ///     __ __ __            __
 ///    / //_//_/_  ______ _/ /
 ///   / ,<  / / | / / __ `/ /
 ///  / /| |/ /| |/ / /_/ / /
 /// /_/ |_/_/ |___/\__,_/_/
-/// ~~~
+/// ```
 #[derive(Debug, Parser)]
 #[argx(name = "kivald", version = SHORT_VERSION, long_version = LONG_VERSION)]
 pub struct Cli {
